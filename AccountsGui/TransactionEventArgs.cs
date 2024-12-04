fecba228f598d9f8e7054c7ee639f73318fcdf3b
mechbrian <mechbrian062@gmail.com>
2024-12-04 16:36:47 -0500

    Brian add TransactionEventArgs

---
+++ AccountsGui/TransactionEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsGui
+{
+    internal class TransactionEventArgs : LoginEventArgs
+    {
+        public decimal Amount { get; }
+        public TransactionEventArgs(string personName, decimal amount, bool success) : base(personName, success)
+        {
+            this.Amount = amount;
+        }
+
+
+    }
+}

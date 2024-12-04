7ada9fdf54e42e8c139d8909820c39bf16dcf301
Ian Roberts <ianr9532@gmail.com>
2024-12-04 16:06:50 -0500

    Added dummy files for logineventargs and savingaccount

---
+++ AccountsGui/SavingAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsGui
+{
+    internal class SavingAccount : Account
+    {
+        public SavingAccount(string type, double balance) : base(type, balance)
+        {
+        }
+
+        public override void PrepareMonthlyStatement()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+

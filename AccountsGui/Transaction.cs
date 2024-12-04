3dce412b541519efa8fd31c79df87c5acb88ac91
mechbrian <mechbrian062@gmail.com>
2024-12-03 21:10:59 -0500

    Add CheckingAccount class and Transaction structure and update csproj file

---
+++ AccountsGui/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsGui
+{
+    public struct Transaction
+    {
+        public string AccountNumber { get; }
+
+        public decimal Amount { get; }
+
+        public Person Originator { get; }
+
+        public DateTime Time { get; }
+
+        public Transaction(string accountNumber, decimal amount, Person person, DateTime time)
+        {
+            AccountNumber = accountNumber;
+            Amount = amount;
+            Originator = person;
+            Time = time;
+        }
+
+        public override string ToString()
+        {
+            return $"Account: {AccountNumber}, Amount: {Amount:C}, Originator: {Originator}, Time: {Time}";
+        }
+    }
+}

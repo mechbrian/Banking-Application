658c8fdefc1da82fadb881c76d41b772d75fe1b5
DarkSkull0 <71239140+DarkSkull0@users.noreply.github.com>
2024-12-05 03:55:48 -0500

    Updated LoginEventArgs.cs
    completed class

--- AccountsGui/LoginEventArgs.cs
+++ AccountsGui/LoginEventArgs.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +11,10 @@ namespace AccountsGui
         public string PersonName { get; }
         public bool Success { get; }
 
-        public LoginEventArgs(string PersonName, bool Success)
+        public LoginEventArgs(string personName, bool success)
         {
-            this.PersonName = PersonName;
-            this.Success = Success;
+            PersonName = personName;
+            Success = success;
         }
     }
 }

341d7e60cb877742824edd7730e85cc10bdb977c
DarkSkull0 <71239140+DarkSkull0@users.noreply.github.com>
2024-12-05 04:49:40 -0500

    Completed SavingAccount.cs
    finished class, implemented all exceptions n such

--- AccountsGui/SavingAccount.cs
+++ AccountsGui/SavingAccount.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,29 +6,70 @@ using System.Threading.Tasks;
 
 namespace AccountsGui
 {
-    public class SavingAccount : Account
+    public class SavingAccount : Account, ITransaction
     {
-        public SavingAccount(decimal balance = 0) : base("SV-",balance)
-        {
-        }
-        public SavingAccount(string type, decimal balance) : base(type, balance)
-        {
-        }
+        private const decimal COST_PER_TRANSACTION = 0.5m;
+        private const decimal INTEREST_RATE = 0.015m;
 
-        public void Deposit(decimal amount, Person person)
-        {
+        public SavingAccount(decimal balance = 0) : base("SV-", balance) { }
 
+        public new void Deposit(decimal amount, Person person)
+        {
+            if (!IsUser(person.Name))
+            {
+                OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, false));
+                throw new AccountException(ExceptionType.NAME_NOT_ASSOCIATED_WITH_ACCOUNT);
+            }
+
+            if (!person.IsAuthenticated)
+            {
+                OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, false));
+                throw new AccountException(ExceptionType.USER_NOT_LOGGED_IN);
+            }
+
+            base.Deposit(amount, person);
+            OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, true));
         }
 
         public void Withdraw(decimal amount, Person person)
         {
+            if (!IsUser(person.Name))
+            {
+                OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, false));
+                throw new AccountException(ExceptionType.NAME_NOT_ASSOCIATED_WITH_ACCOUNT);
+            }
 
+            if (!person.IsAuthenticated)
+            {
+                OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, false));
+                throw new AccountException(ExceptionType.USER_NOT_LOGGED_IN);
+            }
+
+            if (amount > Balance)
+            {
+                OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, false));
+                throw new AccountException(ExceptionType.INSUFFICIENT_FUNDS);
+            }
+
+            base.Deposit(-amount, person);
+            OnTransactionOccur(this, new TransactionEventArgs(person.Name, -amount, true));
         }
 
         public override void PrepareMonthlyStatement()
         {
-            throw new NotImplementedException();
+            try
+            {
+                decimal serviceCharge = this.transactions.Count * COST_PER_TRANSACTION;
+                decimal interest = (LowestBalance * INTEREST_RATE) / 12;
+
+                this.Balance += interest - serviceCharge;
+
+                transactions.Clear();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
-
